adbbb053a312c244cb1d305c1e1231679dddbbbc
manjaroi <703564453@qq.com>
2018-07-31 20:57:52 +0800

    base

---
+++ my-project/src/components/pages/Home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    
+</template>
+
+<script>
+import AppHomeBanner from './HomeBanner';
+
+export default {
+    name : 'AppHome',
+    components : {
+        AppHomeBanner
+    }
+}
+</script>
+
+// lang=scss 表示写的是scss代码
+<style scoped lang = "scss">
+
+</style>
+
+
